d26e91e551fe94e5857e93b537eb2535fbcb5630
loick21 <loicnzenang@gmail.com>
2022-01-14 17:28:51 +0100

    add of compute age function

---
+++ src/computeAge.js
@@ -0,0 +1,9 @@
+const computeAge = (birthday, currentDate) => {
+
+    if(currentDate.getMonth() > birthday.getMonth()) return (currentDate.getFullYear() - birthday.getFullYear());
+    if(currentDate.getMonth() === birthday.getMonth() &&  currentDate.getDay() >= birthday.getDay())
+        return (currentDate.getFullYear() - birthday.getFullYear());
+    return currentDate.getFullYear() - birthday.getFullYear() - 1;
+}
+
+module.exports = computeAge;
