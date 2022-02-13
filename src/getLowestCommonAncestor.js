8ce0bd9c6920eaa98d035d8a6c5bcbe45d1ec34f
loick21 <loicnzenang@gmail.com>
2022-02-14 00:00:09 +0100

    test CI and fix getLowestAncestor

--- src/getLowestCommonAncestor.js
+++ src/getLowestCommonAncestor.js
@@ -1,14 +1,14 @@
 const getAbsolutePath = require("./getAbsolutePath");
 
 const getLowestCommonAncestor = (tree, id1, id2) => {
-    const path1 = getAbsolutePath(id1);
-    const path2 = getAbsolutePath(id2);
+    const path1 = getAbsolutePath(tree,id1);
+    const path2 = getAbsolutePath(tree,id2);
     let result = -1;
     while (path1.length && path2.length) {
-        if (path1[0].id !== path2[0].id) {
-            result = path1.shift().id;
-            break;
-        }
+        if (path1[0] !== path2[0]) break;
+        result = path1[0];
+        path1.shift();
+        path2.shift();
     }
     return result;
 };
--- test/getLowestAncestorPath.test.js
+++ test/getLowestAncestorPath.test.js
@@ -1,13 +1,14 @@
 const getLowestCommonAncestorTest = require("../src/getLowestCommonAncestor.js");
+let getAbs = require("../src/getAbsolutePath.js");
 
-test("getLowestCommonAncestorTest should return : 2 "), () => {
+test(("getLowestCommonAncestorTest should return : 2 "), () => {
     const root = {id: 0, name: "root", parent: -1};
     const folder1 = {id: 1, name: "folder1", parent: 0};
     const folder2 = {id: 2, name: "folder2", parent: 1};
     const folder3 = {id: 3, name: "folder3", parent: 2};
     const folder4 = {id: 4, name: "folder4", parent: 2};
-    const file1 = {id: 5, name: "file1", parent: 2};
-    const file2 = {id: 6, name: "file2", parent: 3};
+    const file1 = {id: 5, name: "file1", parent: 3};
+    const file2 = {id: 6, name: "file2", parent: 4};
     const tree = new Map();
     tree.set(root.id, root);
     tree.set(folder1.id, folder1);
@@ -18,4 +19,4 @@ test("getLowestCommonAncestorTest should return : 2 "), () => {
     tree.set(file2.id, file2);
 
     expect(getLowestCommonAncestorTest(tree, 5, 6)).toStrictEqual(2);
-};
+});
